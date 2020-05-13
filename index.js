8a43532d33d17057092f13b3f4ee0f6b449bb9e1
caz <caz@uniba.jp>
2020-05-13 22:32:26 +0900

    修正

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var http = require('http');
+var http = require("http");
 
-var server = http.createServer(function(request, response) {
+var fs = require('fs');
 
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<html><body><h1>Hello World!</h1></body></html>");
-
+var server = http.createServer(function (request, response) {
+    fs.readFile('./index.html', 'utf-8', function (error, data) {
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.write(data);
+        response.end();
+    });
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
